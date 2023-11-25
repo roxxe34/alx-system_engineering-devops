4bced3bf3e7c0d1d9a0650c2b37e822ea9f5b27d
roxxe34 <hamzafarissi7@gmail.com>
2023-11-25 12:22:55 +0100

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,17 @@
-# install flask using puppet
-#
+# install_flask.pp
 
-package { 'werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package{ 'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
-require  => Package['werkzeug'],
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  require => [Package['python3-pip'], Package['werkzeug']],
 }
 
